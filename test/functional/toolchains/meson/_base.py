8f6998d1898d3dc06becbd813707a2422c33569a
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-22 10:03:10 -0400

    RabeyaMuna__diff__id_544

--- test/functional/toolchains/meson/_base.py
+++ test/functional/toolchains/meson/_base.py
@@ -7,18 +7,20 @@ from conan.test.utils.tools import TestClient
 
 
 @pytest.mark.tool("meson")
-@pytest.mark.skipif(platform.system() not in ("Darwin", "Windows", "Linux"),
-                    reason="Not tested for not mainstream boring operating systems")
+@pytest.mark.skipif(
+    platform.system() not in ("Darwin", "Windows", "Linux"),
+    reason="Not tested for not mainstream boring operating systems",
+)
 class TestMesonBase(unittest.TestCase):
     def setUp(self):
         self.t = TestClient()
 
     def _check_binary(self):
         # FIXME: Some values are hardcoded to match the CI setup
-        host_arch = self.t.get_default_host_profile().settings['arch']
+        host_arch = self.t.get_default_host_profile().settings["arch"]
         arch_macro = {
             "gcc": {"armv8": "__aarch64__", "x86_64": "__x86_64__"},
-            "msvc": {"armv8": "_M_ARM64", "x86_64": "_M_X64"}
+            "msvc": {"armv8": "_M_ARM64", "x86_64": "_M_X64"},
         }
         if platform.system() == "Darwin":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
@@ -33,4 +35,4 @@ class TestMesonBase(unittest.TestCase):
             self.assertIn("main _MSVC_LANG2014", self.t.out)
         elif platform.system() == "Linux":
             self.assertIn(f"main {arch_macro['gcc'][host_arch]} defined", self.t.out)
-            self.assertIn("main __GNUC__9", self.t.out)
+            self.assertIn("main __GNUC__", self.t.out)
