7e308347c6d7b4e386506ee9e79dc2c56348e413
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-22 10:01:27 -0400

    RabeyaMuna__diff__id_532

--- conan/internal/model/cpp_info.py
+++ conan/internal/model/cpp_info.py
@@ -12,7 +12,7 @@ from conan.internal.util.files import load, save
 
 _DIRS_VAR_NAMES = ["_includedirs", "_srcdirs", "_libdirs", "_resdirs", "_bindirs", "_builddirs",
                    "_frameworkdirs", "_objects"]
-_FIELD_VAR_NAMES = ["_system_libs", "_package_framework", "_frameworks", "_libs", "_defines",
+_FIELD_VAR_NAMES = ["_system_libs", "_frameworks", "_libs", "_defines",
                     "_cflags", "_cxxflags", "_sharedlinkflags", "_exelinkflags", "_sources"]
 _ALL_NAMES = _DIRS_VAR_NAMES + _FIELD_VAR_NAMES
 _SINGLE_VALUE_VARS = "type", "exe", "location", "link_location", "languages", "package_framework"
